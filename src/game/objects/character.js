c4c2e5a046a349fd0c90c3e8921268e0064ad9c6
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-02-16 02:26:48 +0500

    Make character non-touchable on death

--- src/game/objects/character.js
+++ src/game/objects/character.js
@@ -35,6 +35,7 @@ export class Character extends Body {
 
   kill() {
     this.collidable = false;
+    this.touchable = false;
     this.release();
     this.dispatchEvent(new Event("death"));
   }
