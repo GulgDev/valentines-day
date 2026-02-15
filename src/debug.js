2b12ac0e4a2719e98f38b8ef003c83363caf8d7a
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-02-16 01:18:09 +0500

    Update first level

--- src/debug.js
+++ src/debug.js
@@ -1,1 +1,1 @@
-export const DEBUG_WIREFRAMES_ENABLED = true;
+export const DEBUG_WIREFRAMES_ENABLED = false; // true
--- src/levels/level1.js
+++ src/levels/level1.js
@@ -1,5 +1,7 @@
-import { whitePaw } from "../../res/index.js";
+import { wool } from "../../res/index.js";
+import { SCREEN_HEIGHT, SCREEN_WIDTH } from "../const/screen.js";
 import { Sprite9Slice } from "../game/objects/9-slice.js";
+import { CHARACTER_SIZE } from "../game/objects/character.js";
 import { fadeIn, fadeOut } from "../transition.js";
 
 export const level1 = {
@@ -8,22 +10,57 @@ export const level1 = {
     { x: 128, y: 0, direction: -1 },
   ],
   init(world) {
+    // Floor
     world.bodies.add(
       Object.assign(
         new Sprite9Slice(
           0,
-          64,
-          512,
-          64,
-          whitePaw,
-          whitePaw,
-          whitePaw,
-          whitePaw,
-          whitePaw,
-          whitePaw,
-          whitePaw,
-          whitePaw,
-          whitePaw,
+          SCREEN_HEIGHT * 0.5 - (SCREEN_HEIGHT - CHARACTER_SIZE) / 4,
+          SCREEN_WIDTH,
+          (SCREEN_HEIGHT - CHARACTER_SIZE) / 2,
+          wool,
+          4,
+          4,
+          4,
+          4,
+        ),
+        {
+          static: true,
+        },
+      ),
+    );
+
+    // Walls
+    world.bodies.add(
+      Object.assign(
+        new Sprite9Slice(
+          SCREEN_WIDTH * -0.5 - (SCREEN_WIDTH - CHARACTER_SIZE) / 4 + 4,
+          0,
+          (SCREEN_WIDTH - CHARACTER_SIZE) / 2,
+          SCREEN_HEIGHT,
+          wool,
+          4,
+          4,
+          4,
+          4,
+        ),
+        {
+          static: true,
+        },
+      ),
+    );
+    world.bodies.add(
+      Object.assign(
+        new Sprite9Slice(
+          SCREEN_WIDTH * 0.5 + (SCREEN_WIDTH - CHARACTER_SIZE) / 4 - 4,
+          0,
+          (SCREEN_WIDTH - CHARACTER_SIZE) / 2,
+          SCREEN_HEIGHT,
+          wool,
+          4,
+          4,
+          4,
+          4,
         ),
         {
           static: true,
