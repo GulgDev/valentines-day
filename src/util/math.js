1ca5de9a9b0878ae5e4ef01ecb5c7eeb7cd73ff8
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-02-17 03:07:39 +0500

    Add level geometry utilities

--- src/util/math.js
+++ src/util/math.js
@@ -12,3 +12,11 @@ export function moveTowards(current, target, maxDistanceDelta) {
     y: current.y + (dy / dist) * maxDistanceDelta,
   };
 }
+
+export function place(x, y, anchorX, anchorY, w, h) {
+  return [x - (anchorX - 0.5) * w, y - (anchorY - 0.5) * h, w, h];
+}
+
+export function stretchY(x, y1, y2, anchorX, w) {
+  return [x - (anchorX - 0.5) * w, (y1 + y2) / 2, w, Math.abs(y2 - y1)];
+}
