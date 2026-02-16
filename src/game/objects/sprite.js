96950e301b17df0ecbbc7b04d11b05fb4b00ac8d
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-02-17 03:20:36 +0500

    Make sprite images offsettable

--- src/game/objects/sprite.js
+++ src/game/objects/sprite.js
@@ -1,13 +1,22 @@
 import { Body } from "./body.js";
 
 export class Sprite extends Body {
+  imgOffsetX = 0;
+  imgOffsetY = 0;
+
   constructor(x, y, w, h, img) {
     super(x, y, w, h);
     this.img = img;
   }
 
   draw(renderer) {
-    renderer.image(this.img, this.x, this.y, this.w, this.h);
+    renderer.image(
+      this.img,
+      this.x + this.imgOffsetX,
+      this.y + this.imgOffsetY,
+      this.w,
+      this.h,
+    );
 
     super.draw(renderer);
   }
