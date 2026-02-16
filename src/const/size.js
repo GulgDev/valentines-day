0b1e6ef13ddf86feed4d1aaf0bb0b725689251d8
GulgDev <65360617+GulgDev@users.noreply.github.com>
2026-02-17 03:45:57 +0500

    Fix size constants

--- src/const/size.js
+++ src/const/size.js
@@ -7,4 +7,4 @@ export const W_SECONDARY = 32;
 export const X_EDGE = SCREEN_WIDTH / 2 - 4;
 export const X_MID = (0 + W_WALL / 2 + X_EDGE) / 2;
 export const X_MID0 = (0 + W_WALL / 2 + X_MID - W_SECONDARY / 2) / 2;
-export const X_MID1 = (X_MID - W_SECONDARY / 2 + X_EDGE) / 2;
+export const X_MID1 = (X_MID + W_SECONDARY / 2 + X_EDGE) / 2;
